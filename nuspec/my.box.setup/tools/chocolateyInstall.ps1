dfe2948d62b27394f5852c8ab0e464489efe581f
Gary Ewan Park <gep13@gep13.co.uk>
2018-11-23 20:24:24 +0000

    Add additional arguments to choco install commands

--- nuspec/my.box.setup/tools/chocolateyInstall.ps1
+++ nuspec/my.box.setup/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
-choco install git
-choco install visualstudio2017community --package-parameters '--locale en-US'
-choco install visualstudio2017-workload-manageddesktop
-choco install visualstudio2017-workload-netcoretools
-choco install vscode
+choco install git -y --no-progress
+choco install visualstudio2017community --package-parameters '--locale en-US' -y --no-progress
+choco install visualstudio2017-workload-manageddesktop -y --no-progress
+choco install visualstudio2017-workload-netcoretools -y --no-progress
+choco install vscode -y --no-progress
 
 # Refresh environment to use tools such as "code" or "git"
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
