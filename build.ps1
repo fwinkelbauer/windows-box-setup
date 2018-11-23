934f4c3c78a6d1d9c6a25922b672fed3eb752369
Manfred Wallner <schusterfredl@mwallner.net>
2018-11-23 22:01:32 +0100

    fix line endings via .gitattributes

--- build.ps1
+++ build.ps1
@@ -3,30 +3,31 @@ $ErrorActionPreference = 'Stop'
 $artifactsDir = '.\artifacts'
 
 function Clear-Artifacts {
-    if (Test-Path $artifactsDir) {
-        Get-ChildItem -Path $artifactsDir -Recurse | Remove-Item -Force -Recurse
-    } else {
-        New-Item -ItemType Directory -Path $artifactsDir
-    }
+  if (Test-Path $artifactsDir) {
+    Get-ChildItem -Path $artifactsDir -Recurse | Remove-Item -Force -Recurse
+  }
+  else {
+    New-Item -ItemType Directory -Path $artifactsDir
+  }
 }
 
 function Copy-Boxstarter {
-    Import-Module Boxstarter.Chocolatey
-    Copy-Item -Path $Boxstarter.BaseDir -Destination $artifactsDir -Recurse -Exclude '*.nupkg'
+  Import-Module Boxstarter.Chocolatey
+  Copy-Item -Path $Boxstarter.BaseDir -Destination $artifactsDir -Recurse -Exclude '*.nupkg'
 }
 
 function Build-Packages {
-    foreach ($nuspec in Get-ChildItem -Path '.\nuspec\**\*.nuspec') {
-        choco pack $nuspec --output-directory "$artifactsDir\Boxstarter\BuildPackages"
-    }
+  foreach ($nuspec in Get-ChildItem -Path '.\nuspec\**\*.nuspec') {
+    choco pack $nuspec --output-directory "$artifactsDir\Boxstarter\BuildPackages"
+  }
 }
 
 function Copy-InstallScripts {
-    Copy-Item -Path '.\scripts\*' -Destination $artifactsDir
+  Copy-Item -Path '.\scripts\*' -Destination $artifactsDir
 }
 
 function Compress-Artifacts {
-    Compress-Archive -Path "$artifactsDir\*" -DestinationPath "$artifactsDir\box.zip"
+  Compress-Archive -Path "$artifactsDir\*" -DestinationPath "$artifactsDir\box.zip"
 }
 
 Clear-Artifacts
