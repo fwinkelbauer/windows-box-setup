7d22eaba60048cdd1e38b3a83625d3c894631a13
Manfred Wallner <schusterfredl@mwallner.net>
2018-11-24 09:24:22 +0100

    pause only on error

--- scripts/install-mybox.bat
+++ scripts/install-mybox.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
-PAUSE
+if NOT ["%errorlevel%"]==["0"] (
+    PAUSE
+    exit /b %errorlevel%
+)
