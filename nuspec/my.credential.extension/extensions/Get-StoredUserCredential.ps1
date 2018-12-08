0305b4a596b28c0d4db63d1c1d49f8a8b4f3bee1
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-12-08 10:58:04 +0100

    Add the my.credential.extension package

---
+++ nuspec/my.credential.extension/extensions/Get-StoredUserCredential.ps1
@@ -0,0 +1,21 @@
+function Get-StoredUserCredential {
+    $credFile = Join-Path $env:UserProfile 'cred.xml'
+
+    if (Test-Path $credFile) {
+        return Import-CliXml -Path $credFile
+    }
+
+    $currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $cred = Get-Credential $currentUser
+
+    try {
+        # Validate the credential object
+        Start-Process 'cmd.exe' -ArgumentList '/c', 'echo' -Credential $cred
+
+        $cred | Export-CliXml -Path $credFile
+
+        return $cred
+    } catch {
+        throw "Invalid password for user '$currentUser'"
+    }
+}
