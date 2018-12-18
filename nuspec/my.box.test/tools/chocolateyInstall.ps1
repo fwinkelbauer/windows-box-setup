eb5ca4b69ca9ab6931e2b3345d4e73e4903705f8
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-12-18 16:01:16 +0100

    Turn my.test.package into a box package

---
+++ nuspec/my.box.test/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+once 'you only list once' {
+    choco list -lo
+}
+
+# 7z.exe is installed via Chocolatey. See C:\ProgramData\chocolatey\bin
+exec {
+    7z this-is-not-the-error-you-are-looking-for
+} -validExitCodes @(0, 7)
+
+try {
+    exec {
+        7z some-invalid-argument
+    }
+} catch {
+    Write-Warning 'The exec statement failed. This is expected.'
+}
