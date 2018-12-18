0d27d46de50337aed546c1140fcd2f5033e04130
Florian Winkelbauer <mail@florianwinkelbauer.com>
2018-12-18 20:25:26 +0100

    Fix formatting in my.vs2017.cake

--- nuspec/my.vs2017.cake/tools/chocolateyInstall.ps1
+++ nuspec/my.vs2017.cake/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 $parameters = @{
-    PackageName   = $env:ChocolateyPackageName
-    VsixUrl       = 'https://vs-publisher-1392591.gallerycdn.vsassets.io/extensions/vs-publisher-1392591/cakeforvisualstudio/0.2.0.0/1503436806448/229353/4/Cake.VisualStudio.vsix'
-    Checksum      = '752A01E2A40A5DED7BED6FC16930B347205030AE693C75D3D5DC76DCCE407D02'
-    ChecksumType  = 'sha256'
+    PackageName = $env:ChocolateyPackageName
+    VsixUrl = 'https://vs-publisher-1392591.gallerycdn.vsassets.io/extensions/vs-publisher-1392591/cakeforvisualstudio/0.2.0.0/1503436806448/229353/4/Cake.VisualStudio.vsix'
+    Checksum = '752A01E2A40A5DED7BED6FC16930B347205030AE693C75D3D5DC76DCCE407D02'
+    ChecksumType = 'sha256'
 }
 
 Install-VisualStudioVsixExtension @parameters
--- nuspec/my.vs2017.cake/tools/chocolateyUninstall.ps1
+++ nuspec/my.vs2017.cake/tools/chocolateyUninstall.ps1
@@ -1,8 +1,8 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop'
 
 $parameters = @{
-    PackageName   = $env:ChocolateyPackageName
-    VsixId        = '3cf9b016-d63f-44ee-849d-6f3efc996134'
+    PackageName = $env:ChocolateyPackageName
+    VsixId = '3cf9b016-d63f-44ee-849d-6f3efc996134'
 }
 
 Uninstall-VisualStudioVsixExtension @parameters
